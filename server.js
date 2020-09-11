2b218fa75e2925093a661019493df28e12b083e2
Gaetan.gerard <gaetan.gerard@data-essential.com>
2020-09-11 12:40:15 +0200

    Add port to server.js

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const { typeDefs } = require('./graphql/schema/schema.js');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server is running at ${url}`);
 });
