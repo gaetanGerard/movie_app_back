952c44107915ba415f1245374df692ecdb990166
Gaetan.gerard <gaetan.gerard@data-essential.com>
2020-09-11 14:35:03 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const { ApolloServer } = require('apollo-server');
-// require('dotenv').config();
+require('dotenv').config();
 
 // Import Resolvers and Schema
 const { resolvers } = require('./graphql/resolvers/index.js');
 const { typeDefs } = require('./graphql/schema/schema.js');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  engine: {
+    reportSchema: true,
+    variant: 'current',
+  },
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server is running at ${url}`);
